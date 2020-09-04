a31f7c2a799e3b562e4ba279746bd0a4b1b0e1be
RebeccaRoach <rebeccaroach12@gmail.com>
2020-09-03 17:36:44 -0700

    Implements stacks methods

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,19 @@
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @list = LinkedList.new
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    @list.add_first(element)
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    # return nil if @list.empty?
+    @list.remove_first
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @list.empty?
   end
 
   def to_s
