66399c498e07e909b0874b3ef67d1456e7cef324
quinqu <quinruby0@gmail.com>
2020-09-04 11:03:00 -0700

    balanced

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,25 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  pairs = {"{" => "}", "[" => "]" , "(" => ")"}
+  arr = string.split('')
+  if arr.length % 2 != 0 
+    return false
+  end 
+  
+  stack = Stack.new
+  n = arr.length
+  n.times do |i|
+    if arr[i] == "[" || arr[i] == "{" || arr[i] == "("
+      stack.push(arr[i])
+    else
+      top = stack.pop
+      if arr[i] != pairs["#{top}"]
+        return false 
+      end 
+    end 
+  end 
+  return stack.empty?
 end
 
 # Time Complexity: ?
