e1b6805e5875b6eb5ed6c5247a42a0d513a47ee3
Leah <leahwhughes@gmail.com>
2020-09-03 19:50:41 -0700

    initial queue methods - not buffered yet

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,60 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @queue = Array.new
+    @front = 0
+    @back = 0
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # puts "Queue before enqueuing: #{@queue}, @back: #{@back}"
+    @queue[@back] = element
+    @back += 1
+    # puts "Queue after enqueuing: #{@queue}, @back: #{@back}"
+
+    # add logic for overflow/wrapping
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    return nil if self.empty?
+    # puts "Queue before dequeuing: #{@queue}, @front: #{@front}"
+
+    dequeued = @queue[@front]
+    @queue[@front] = nil
+    @front += 1
+    # puts "Queue after dequeuing: #{@queue}, @front: #{@front}"
+    # add logic for overflow/wrapping
+    
+    return dequeued
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return nil if @queue.empty?
+    return @queue[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return 0 if @queue.nil?
+
+    count = 0
+    @queue.each do |item|
+      count += 1 if item != nil
+    end
+
+    return count
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return false if @queue.nil?
+
+    @queue.each do |item|
+      return false if item != nil
+    end
+
+    return true
   end
 
   def to_s
-    return @store.to_s
+    return @queue.compact.to_s
   end
 end
