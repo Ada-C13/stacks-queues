a69574fef83cb4feda7122ad28a537115746ad76
Leah <leahwhughes@gmail.com>
2020-09-05 11:32:49 -0700

    adds balanced method

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,28 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string.empty?
+
+  stack = Stack.new
+  openers = ["(", "[", "{"]
+  closers = [")", "]", "}"]
+  valid_pairs = {
+    "(" => ")",
+    "[" => "]",
+    "{" => "}",
+  }
+  
+  string.each_char do |char|
+    if openers.include?(char) # char is opener
+      stack.push(char) # put char on stack
+    else # char is closer, check in hash for good pair
+      popped = stack.pop
+      return false if valid_pairs[popped] != char
+    end
+  end
+
+  return true if stack.empty?
+  return false
 end
 
 # Time Complexity: ?
