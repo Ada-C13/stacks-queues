4be0a09a86e9ffa40a6633554e9bf8605d65cb14
cojenco <icathyouyang@gmail.com>
2020-09-05 15:14:45 -0700

    pass all tests yay

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,13 +1,68 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is the length of the string
+# Space Complexity: O(n) for the extra stack, where n is the length of the string (or the number of open parens)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return false if string.length % 2 != 0
+  opening = ["(", "[", "{"]
+  closing = [")", "]", "}"]
+  stack = Stack.new
+
+  i = 0
+  while i < string.length
+    parens = string[i]
+    if opening.include?(parens)
+      stack.push(parens)
+    elsif closing.include?(parens)
+      item = stack.pop
+      return false if closing.index(parens) != opening.index(item)
+    else
+      return false
+    end
+    i += 1
+  end
+
+  return stack.empty?
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  operators = ["+", "-", "*", "/", "%"]
+  operands = Stack.new
+  # results = Stack.new
+
+  i = 0
+  while i < postfix_expression.length
+    char = postfix_expression[i]
+    if operators.include?(char)
+      operator = char
+      num2 = operands.pop
+      num1 = operands.pop
+      result = calculate_helper(operator, num1, num2)
+      operands.push(result)
+    else
+      operands.push(char.to_i)
+    end
+    i += 1
+  end
+  
+  ans = operands.pop
+  # puts ans
+  return ans
+end
+
+def calculate_helper(operator, num1, num2)
+  case operator
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
+  when "%"
+    return num1 % num2
+  end
 end
--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,51 @@
 class Queue
 
-  def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+  def initialize(size=20)
+    @store = Array.new
+    @size = size
+    @front = 0
+    @back = 0
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == (@back + 1) % @size    #in cases where queue is full
+      raise StandardError.new("Queue is full")
+    else
+      @store[@back] = element
+      @back = (@back + 1) % @size
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    return nil if self.empty?          #in cases where queue is empty?
+    # FIFO
+    item = @store[@front]
+    @front = (@front + 1) % @size
+    return item
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @front
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @size
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == @back
   end
 
   def to_s
-    return @store.to_s
+    return [] if @front == @back
+    queue = []
+    current = @front
+
+    while current != @back
+      queue << @store[current]
+      current = (current + 1) % @size
+    end
+
+    return queue.to_s
   end
 end
