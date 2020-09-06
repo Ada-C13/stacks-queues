325f7096891a0ac81a6fccf64760ff7dcb789cc5
Jessica - Ada <jessica-ada@osxltTML86.local>
2020-09-05 19:49:30 -0700

    queues

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,62 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @queue = Array.new
+    @size = 20
+    @head = -1
+    @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if (@head == -1)
+      @head = 0
+      @rear = 0
+      @queue[@rear] = element
+    elsif ((@rear + 1) % @size == @front)
+      puts 'full'
+    else
+      @rear = (@rear + 1) % @size
+      @queue[@rear] = element
+    end
+
+    puts "enq: #{@queue.to_s}, #{@head}, #{@rear}"
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if (@head == -1)
+      puts 'empty'
+    elsif @head == @rear
+      temp = @queue[@head]
+      @head = -1
+      @rear = -1
+       puts "de: #{@queue.to_s}, #{@head}, #{@rear}"
+
+      return temp
+    else
+      temp = @queue[@head]
+      @head = (@head + 1) % @size
+      puts "de2: #{@queue.to_s}, #{@head}, #{@rear}"
+      return temp
+    end
   end
 
-  def front
-    raise NotImplementedError, "Not yet implemented"
+  def head
+    return @queue[@head]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @size
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return (@head == @rear) && (@head == -1)
   end
 
   def to_s
-    return @store.to_s
+    if @head <= @rear
+      return @queue[@head..@rear].to_s
+    else
+      return (@queue[@head..20] + @queue[0..@rear]).to_s
+    end
   end
 end
