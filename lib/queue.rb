dc615e26309fae77f61b7d69f3d99b6ca189d8a2
SuelyBarreto <suely@barreto.us>
2020-09-05 20:15:10 -0700

    Tests passing for waves 1, 2, 3

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,45 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @max = 20
+    @store = Array.new(@max)
+    @front = 0
+    @back = 0
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    return nil if @front == (@back + 1) % @max # checking if queue is full
+    current = @back # save @back position before we change it
+    @store[current] = element
+    @back = (@back + 1) % @max # handles case where back hits the end
+    return element
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    return nil if @front == @back # checking if queue is empty
+    current = @front # save @front position before we change it
+    @front = (@front + 1) % @max # handles case where front hits the end
+    return @store[current]
   end
 
-  def front
-    raise NotImplementedError, "Not yet implemented"
+  def front # peek
+    return nil if @front == @back # checking if queue is empty
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return 0 if @front == @back # checking if queue is empty
+    return @back - @front + 1 if @back > @front # @back ahead of @front
+    return (@max - @front) + (@back + 1) # @front ahead of @back
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == @back
   end
 
   def to_s
-    return @store.to_s
+    return [].to_s if @front == @back # checking if queue is empty
+    return @store[@front..@back - 1].to_s if @back > @front # @back ahead of @front
+    return (@store[@front..@max - 1] + @store[0..@back - 1]).to_s # @front ahead of @back
   end
-end
+end # what if @back zero?
