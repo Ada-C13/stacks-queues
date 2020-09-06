a50664c88701c9f1e90639bc89c0fca4ca737323
Maryam Heshmati <mairheshmati@gmail.com>
2020-09-06 13:12:04 -0700

    implemented balanced paran problems

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,28 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  string.each_char do |char|
+    if char == "(" || char == "{" || char == "["
+      stack.push(char)
+    else
+      if stack.empty?
+        return false 
+      else
+        last = stack.pop
+        if char == ")"
+          return false if last != "("
+        elsif char == "}"
+          return false if last != "{"
+        elsif char == "]"
+          return false if last != "["
+        end
+      end
+    end
+  end
+  return stack.empty?
 end
 
 # Time Complexity: ?
