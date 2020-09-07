fa7282898618ddf903ec903cb0a79289e9fe73ab
Haben Foto (Third Sector New England Inc) <v-habenfoto@microsoft.com>
2020-09-07 16:04:28 -0700

    added balanced method

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,26 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+ 
+  opening_brackets = Stack.new
+  string.each_chars do |char|
+    # if an opening bracket, push it into the opening_brackets
+    if char == '{' || char == '(' || char == '['
+      opening_brackets.push(char)
+    else
+      # compare if the last opening bracket matches with the current closing bracket in char
+      opening_bracket = opening_brackets.pop()
+        if char == '}' && opening_bracket != '{'
+          return false
+        elsif char == ')' && opening_bracket != '('  
+          return false
+        elsif char == ']' && opening_bracket != '['
+          return false
+        end
+    end
+  end
+  # if everything matches/closes, then we have a balanced brackets
+  return true
 end
 
 # Time Complexity: ?
