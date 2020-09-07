25611ccf90db0a7379393cb0c7dbd4bf6aaa40bf
Corinna Fabre <corinna.fabre@gmail.com>
2020-09-06 18:34:17 -0700

    Passes problems tests

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,27 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string == ""
+  
+  pairs = {
+    "}" => "{",
+    ")" => "(",
+    "]" => "["
+  }
+
+  trapper_keeper = Stack.new
+
+  string.each_char do |char|
+    if pairs.values.include?(char)
+      trapper_keeper.push(char)
+    else
+      last_in = trapper_keeper.pop
+      return false if last_in != pairs[char]
+    end
+  end
+
+  return true if trapper_keeper.empty?
+  return false
 end
 
 # Time Complexity: ?
