cff25364b8f3c143babef58dd87250cb07847589
Nataliya Pogodina <n.pogodina@gmail.com>
2020-09-06 20:44:07 -0700

    Implemented Queue using Array with circular buffer

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,86 @@
 class Queue
-
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(size = 20)
+    @size = size
+    @front = -1
+    @rear = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @front = 0
+      @rear = 0
+      @store[@rear] = element
+    elsif (@front == 0 && @rear == size - 1)
+      raise StandardError.new("The queue is full")
+    elsif (@rear == @front - 1)
+      raise StandardError.new("The queue is full")
+    else
+      @rear = (@rear + 1) % size
+      @store[@rear] = element
+
+    # Covers 2 scenarios:
+    # elsif (@front > 0 && @rear == size - 1)
+    #   @rear = 0
+    #   @store[@rear] = element
+    # else
+    #   @rear += 1
+    #   @store[@rear] = element
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      raise StandardError.new("The queue is empty")
+    end
+
+    removed = @store[@front]
+    @store[@front] = nil
+
+    if @front == @rear
+      @front = -1
+      @rear = -1
+    else
+      @front = (@front + 1) % size
+
+    # Covers 2 scenarios:  
+    # elsif @front == size - 1
+    #   @front = 0
+    # else
+    #   @front += 1
+    end
+
+    return removed
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    @size
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == -1 && @rear == -1 ? true : false
   end
 
   def to_s
-    return @store.to_s
+    result = []
+    i = @front
+    while i < size && @store[i] != nil
+      result << @store[i]
+      i += 1
+    end
+
+    if @rear < @front
+      i = @rear
+      while i < @front && @store[i] != nil
+        result << @store[i]
+        i += 1
+      end
+    end
+
+    return result.to_s
   end
 end
