c7008dabccc1990d9d7c445ec052ae726b1b4b1d
mulhoo <oliviamulholland94@gmail.com>
2020-09-08 16:41:18 -0700

    Wave 3 complete, tests pass

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,13 +1,25 @@
-require_relative './stack.rb'
+require_relative 'stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: on
+# Space Complexity: on
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  pairs = {
+    ')' => '(',
+    '}' => '{',
+    ']' => '['
+  }
+
+  stack = Stack.new
+
+  string.split('').each do |char|
+    !pairs[char] ? stack.push(char) : (return false if pairs[char] != stack.pop())
+  end
+
+  return stack.empty?
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+
 end
