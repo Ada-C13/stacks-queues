4bd9b458813e391f133785f5987f6b15f0bcb98e
Haben Foto (Third Sector New England Inc) <v-habenfoto@microsoft.com>
2020-09-07 17:13:49 -0700

    removed comments

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,17 +1,13 @@
 class Stack
   def initialize
     @store = LinkedList.new
-    # raise NotImplementedError, "Not yet implemented"
   end
 
   def push(element)
-    # raise NotImplementedError, "Not yet implemented"
     @store.add_first(element)
   end
 
   def pop()
-    # raise NotImplementedError, "Not yet implemented"
-
     return nil if self.empty? 
 
     element = @store.remove_first
@@ -20,7 +16,6 @@ class Stack
   end
 
   def empty?()
-    # raise NotImplementedError, "Not yet implemented"
     return @store.empty?
   end
 
