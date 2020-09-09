426792a7027c06162a67af6981fa5a505ddd5833
thenora <thenora@gmail.com>
2020-09-08 22:51:01 -0700

    added brackets psuedo code

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,21 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+# Given a string containing opening and closing braces, check if it represents a balanced expression or not.
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  string.each_char do |char|
+    # if character is an opening bracket
+      # push to stack
+    # if not
+      # pop the top character from the stack
+        # if the current character is a closing bracket
+          # return false if the top stack character is not a matching open
+
+  # return true if the stack is empty, false if not
+  end
 end
 
 # Time Complexity: ?
