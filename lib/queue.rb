d1ca58a5a353b6e2a9d1a4b80e38e89c77dba8b7
dnguye2 <dnguyen1093@gmail.com>
2020-09-08 22:05:58 -0700

    queue class methods completed

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,61 @@
 class Queue
 
-  def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+  attr_accessor :ring, :front, :back, :size
+
+  def initialize(size=20)
+    @size = size
+    @ring = Array.new(size)
+    @front = 0
+    @back = 0
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # if circular buffer is full, return empty array
+    return [] if (@back + 1) % @size == @front
+
+    # otherwise, add element to the back
+    @ring[@back] = element
+
+    # change @back to next free position, clockwise
+    @back = (@back + 1) % @size
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    return [] if @ring.empty?
+
+    dequeued_element = @ring[@front]
+    # dequeue by reassigning the front to the next position, clockwise
+    @front = (@front + 1) % @size
+
+    return dequeued_element
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    @front
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    @size
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    @back == @front
   end
 
   def to_s
-    return @store.to_s
+    return [] if @ring.empty?
+
+    ring_output = []
+    current_element = @front
+
+    while current_element != @back
+      # only get elements that are not nil
+      ring_output << @ring[current_element]
+
+      # shift to next element clockwise
+      current_element = (current_element + 1) % @size
+    end
+
+    return ring_output.to_s
   end
 end
