f31020c8f5a11c640630e7072565e0d8c16a8ab0
dnguye2 <dnguyen1093@gmail.com>
2020-09-08 22:58:42 -0700

    completed time and space complexity

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,18 +1,21 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n), will always have to traverse through the string
+# Space Complexity: O(n), because in the worst case all char in string
+# are opening brackets, best case would be O(1) where we push and pop every item
 def balanced(string)
   return false if string.nil?
 
   string_stack = Stack.new
+
+  # Traversal of string
   string.each_char { |char|
+    # push to stack if the character is an opening brace
     if char == '{' or char == '[' or char == '('
-      # push if opening brace
       string_stack.push(char)
     else
-      # pop opening brace and compare 
-      # char to return true or false
+      # pop brace from stack and compare 
+      # char, return false if character doesn't match
       current_char = string_stack.pop()
       case current_char
       when '('
@@ -25,8 +28,8 @@ def balanced(string)
     end
   }
 
-  # if stack has any characters, not balanced
-  # so we return true
+  # if stack is empty, return true
+  # else return false since it's unbalanced
   string_stack.empty? ? true : false
 end
 
