c035d7f87d2d70a3a84c9be9f0c802a9c7ce6e4d
dnguye2 <dnguyen1093@gmail.com>
2020-09-08 22:45:47 -0700

    completed balanced method

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,31 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return false if string.nil?
+
+  string_stack = Stack.new
+  string.each_char { |char|
+    if char == '{' or char == '[' or char == '('
+      # push if opening brace
+      string_stack.push(char)
+    else
+      # pop opening brace and compare 
+      # char to return true or false
+      current_char = string_stack.pop()
+      case current_char
+      when '('
+        return false if char != ')'
+      when '{'
+        return false if char != '}'
+      when '['
+        return false if char != ']'
+      end
+    end
+  }
+
+  # if stack has any characters, not balanced
+  # so we return true
+  string_stack.empty? ? true : false
 end
 
 # Time Complexity: ?
