84f1240e3da2f5f267c4f63914c7c4e1840f6384
Alex Robertson <arobertson335@gmail.com>
2020-09-08 22:35:55 -0700

    Adds stacks and queues methods.

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,67 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @queue = Array.new
+    @size = 20
+    @front = -1
+    @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      @front = 0
+      @queue[@front] = element
+      @back = 1
+    elsif @front == @back
+      raise ArgumentError, "The queue is full; cannot enqueue at this time."
+    else
+      @queue[@back] = element
+      @back = (1 + @back) % @size 
+    end
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == -1
+      raise ArgumentError, "The queue is empty."
+    elsif @front == @back
+      interim = @queue[@front]
+      @front = -1
+      @back = -1
+
+      return interim 
+    else
+      interim = @queue[@front]
+      @front = (1 + @front) % @size
+
+      return interim
+    end 
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @queue[@front]
+  end
+  
+  @private
+  def get_queue_contents()
+    if @queue.empty?
+      return []
+    elsif @front > @back
+      return @queue[@front...@size] + @queue[0...@back]
+    else
+      return @queue[@front...@back]
+    end
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    self.get_queue_contents.length
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return self.get_queue_contents.empty?
   end
 
   def to_s
-    return @store.to_s
+    return self.get_queue_contents.to_s
   end
+
 end
