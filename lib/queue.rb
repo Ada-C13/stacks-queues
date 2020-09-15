199e825eccdbfd4c5138890de9fc0084de878c4e
Alicia Combs <alicia.combs.92@gmail.com>
2020-09-14 19:58:08 -0700

    Uncommented code and passing more queue tests

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,19 +1,19 @@
 class Queue
-  # attr_reader :front, :size
+  # attr_reader :first, :size
 
   def initialize
     @store = Array.new
     @size = 20
-    @front = 0
-    @back = 0
+    @first = 0
+    @last = 0
   end
 
   def enqueue(element)
-    if @front == (@back + 1) % @size
+    if @first == (@last + 1) % @size
       raise Error.new("Queue is full")
     else
-      @store[@back] = element
-      @back = (@back + 1) % @size
+      @store[@last] = element
+      @last = (@last + 1) % @size
     end
   end
 
@@ -21,16 +21,17 @@ class Queue
     return nil if self.empty?
 
     element = @store[@first]
-    # @store[@first] = nil
-    @front = (@front + 1) % size
+    @store[@first] = nil
+    @first = (@first + 1) % size
     @last = (@last - 1) % @size
     return element
+
   end
 
 
   #can be reduced to attr_reader??? maybe
-  def front
-    return @front
+  def first
+    @store[@first]
   end
 
   def size
@@ -38,7 +39,7 @@ class Queue
   end
 
   def empty?
-    return @front == @back
+    return @first == @last
   end
 
   def to_s
