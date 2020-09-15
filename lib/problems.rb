bc6fc4593b5b385c2670ac6f2c4ac79f02c16f23
Alicia Combs <alicia.combs.92@gmail.com>
2020-09-14 20:44:43 -0700

    Began balanced method, pass some tests

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,25 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def balanced(string)
+  return false if string.length % 2 != 0
   
+  struct = Stack.new
+  left_half = ["(", "[", "{"]
+  right_half = [")", "]", "}"]
+
+  left = 0
+  right = string.length - 1
+  until left >= right
+    index = left_half.find_index { |el| el == string[left] }
+
+    return false if string[right] != right_half[index]
+    left += 1
+    right -= 1
+  end
+
+  true
 end
 
 # Time Complexity: ?
