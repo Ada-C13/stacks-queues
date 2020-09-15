4ce7855c09b62ddba7da7dcd8c82574e622dbf9a
Alicia Combs <alicia.combs.92@gmail.com>
2020-09-14 20:21:13 -0700

    Implementing to_s

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,6 +1,4 @@
 class Queue
-  # attr_reader :first, :size
-
   def initialize
     @store = Array.new
     @size = 20
@@ -9,26 +7,21 @@ class Queue
   end
 
   def enqueue(element)
-    if @first == (@last + 1) % @size
-      raise Error.new("Queue is full")
-    else
-      @store[@last] = element
-      @last = (@last + 1) % @size
-    end
+    return nil if @first == (@last + 1) % @size
+
+    @store[@last] = element
+    @last = (@last + 1) % @size
   end
 
   def dequeue
     return nil if self.empty?
 
     element = @store[@first]
-    # @store[@first] = nil
     @first = (@first + 1) % size
 
     return element
   end
 
-
-  #can be reduced to attr_reader??? maybe
   def first
     @store[@first]
   end
@@ -42,6 +35,16 @@ class Queue
   end
 
   def to_s
-    return @store.to_s
+    return nil if self.empty?
+
+    string_queue = Array.new
+    current_element = @first
+
+    while current_element < @last
+      string_queue << @store[current_element]
+      current_element = (current_element + 1) % @size
+    end
+
+    return string_queue.to_s
   end
 end
