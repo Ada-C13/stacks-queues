41d4ff8171dde01157eec49133fa7d01f58b7b6c
yesenia.torres3 <yesenia.torres3@nordstorm.com>
2020-09-30 01:36:11 -0700

    passed wave 3 tests

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,21 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  string_array = string.split('')
+  pair_hash = { "}" => "{", "]" => "[", ")" => "(" }
+
+  string_array.each do |char|
+    if !pair_hash[char]
+      stack.push(char)
+    elsif stack.pop != pair_hash[char]
+      return false
+    end
+  end
+
+  return stack.empty?
 end
 
 # Time Complexity: ?
