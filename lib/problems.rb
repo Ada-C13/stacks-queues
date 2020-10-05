bb3c971a4398d24ca91f7bb8362c98bb593bb8bd
Hannah Tekié <hannahtekie@gmail.com>
2020-10-04 18:48:09 -0700

    all test passed

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,11 +3,50 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return false if string.length % 2 != 0
+ hash = {")"=>"(", "]"=>"[", "}"=>"{"}
+ stack = Stack.new
+
+ string.split("").each do |char|
+  stack.push(char) if !hash[char]
+  if hash[char] 
+   removed = stack.pop
+   return false if hash[char] != removed
+  end
+ end
+return stack.empty?
+
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  postfix_expression.split("").each do |char|
+    if char == "+" 
+      poppednum1 = stack.pop
+      poppednum2 = stack.pop
+      addednum = poppednum1 + poppednum2
+      stack.push(addednum)
+    elsif char == "-" 
+        poppednum1 = stack.pop
+        poppednum2 = stack.pop
+        subtractednum = poppednum2 - poppednum1
+        stack.push(subtractednum)
+    elsif char == "*" 
+        poppednum1 = stack.pop
+        poppednum2 = stack.pop
+        multiplieddnum = poppednum1 * poppednum2
+        stack.push(multiplieddnum)
+    elsif char == "/" 
+        poppednum1 = stack.pop
+        poppednum2 = stack.pop
+        dividednum = poppednum2/poppednum1
+        stack.push(dividednum)
+    else 
+      stack.push(char.to_i)
+    end
+  end
+  return stack.pop
 end
+
